2679a95356412be4187c843a1ecb6fa9bb4e7b89
Rehbein31 <Aaron.Hoehn@web.de>
2024-01-16 14:15:03 +0100

    Fixed Balloon Scene
    + BalloonMovementController | + Scenechange | + FireParticle | + Tree Assets

--- SavingAnkorWat/Assets/HotAirBalloon Assets/scripts/DescendBalloon.cs
+++ SavingAnkorWat/Assets/HotAirBalloon Assets/scripts/DescendBalloon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DescendBalloon : MonoBehaviour  //reversed the balloon movement to make exterior move towards balloon because lever spackte rum wenn balloon moved instead
 {
+    /*
     public BunsenBurnerLever LeverScript;
 
     public GameObject HotAirBalloon;
@@ -29,6 +30,38 @@ public class DescendBalloon : MonoBehaviour  //reversed the balloon movement to 
         else
         {
             this.transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+        }
+    }
+    */
+
+    public BunsenBurnerLever LeverScript;
+
+    public GameObject HotAirBalloon;
+
+    [SerializeField] private GameObject playerController;
+    private CharacterController characterController;
+    private float speed = 1.0f;
+
+
+
+    private void Awake()
+    {
+        characterController = playerController.GetComponent<CharacterController>();
+        characterController.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(HotAirBalloon.transform.position, this.transform.position) < 50)
+        {
+            LeverScript.reachGoal = true;
+
+            HotAirBalloon.transform.position = Vector3.MoveTowards(HotAirBalloon.transform.position, this.transform.position, speed * Time.fixedDeltaTime);
+
+        }
+        else
+        {
+            HotAirBalloon.transform.Translate(new Vector3(1, 0, 0) * speed * Time.fixedDeltaTime);
         }
     }
 }
