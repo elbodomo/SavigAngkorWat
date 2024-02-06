a5d168de780c3b4e5d8491e25bf052938dfbaa2b
pixel-cookie <abersu@gmail.com>
2024-02-06 06:44:06 +0100

    Stone, Anim&Event, NewCollider
    Added new collider, new stone, animation, events (in trigger) to move stone.. Note: idk why my stage also includes changes in "Elias Assets" and "HotAirBaloonScene"

---
+++ SavingAnkorWat/Assets/BersuTestAssets/Codes/NewLeverBehavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewLeverBehavior : MonoBehaviour
+{
+    /*
+    //was gonna add animator here but no need after making it with events lol 
+
+    [SerializeField] private UnityEvent LeverWasUsed;
+
+    [SerializeField] private Transform stoneTransform;
+    [SerializeField] private Transform stoneOrientationTransform;
+
+    private Vector3 stoneOrientationStartPos;
+    private Vector3 stonePositionStart;
+    private float leverHeightValue;
+
+    private void Awake()
+    {
+        stoneOrientationStartPos = stoneOrientationTransform.position;
+        stonePositionStart = stoneTransform.position;
+    }
+
+    private void Update()
+    {
+        if (!stoneTransform.gameObject.activeSelf) return;
+        leverHeightValue = stoneOrientationTransform.position.y - stoneOrientationStartPos.y;
+        stoneTransform.position = new Vector3(stoneTransform.position.x, stonePositionStart.y + leverHeightValue, stoneTransform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out LeverTrigger leverTrigger))
+        {
+            LeverWasUsed.Invoke();
+        }
+    }
+    */
+}
