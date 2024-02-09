0ede70399b20e678d2da39450a38e8b658359b6d
elbodomo <elbobo@live.de>
2024-02-09 16:31:55 +0100

    more fixes, final before voice lines

--- SavingAnkorWat/Assets/EliasTestAssets/Scripts/BiHolder.cs
+++ SavingAnkorWat/Assets/EliasTestAssets/Scripts/BiHolder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BiHolder : MonoBehaviour
 {
     public GameObject Binocular;
+    public bool doRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,8 @@ public class BiHolder : MonoBehaviour
     }
     public void resetPos() {
         Binocular.transform.position = this.gameObject.transform.position;
+        if (doRotation) { 
         Binocular.transform.rotation = this.gameObject.transform.rotation;
+        }
     }
-
 }
